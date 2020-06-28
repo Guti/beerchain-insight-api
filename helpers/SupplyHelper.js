27864e7d5a853db3d9a44ff3734ed971181c1bd1
Guti <mywebrank@gmail.com>
2020-06-28 11:12:27 +0200

    premine

--- helpers/SupplyHelper.js
+++ helpers/SupplyHelper.js
@@ -1,20 +1,18 @@
 var BigNumber = require('bignumber.js');
 
 module.exports = {
-    /**
-     *
-     * @param {Number} height
-     * @return {BigNumber}
-     */
-    getTotalSupplyByHeight: function (height) {
+  /**
+   *
+   * @param {Number} height
+   * @return {BigNumber}
+   */
+  getTotalSupplyByHeight: function (height) {
+    var premine = 210000000;
 
-    	var premine = 1050000;
-
-    	if(height <= 2) {
-    		return (new BigNumber(0)).plus(height * premine);
-    	}
-
-        return (new BigNumber(0)).plus(height * 25).plus(2 * premine);
+    if (height <= 2) {
+      return new BigNumber(0).plus(height * premine);
     }
 
+    return new BigNumber(0).plus(height * 3500).plus(2 * premine);
+  },
 };
